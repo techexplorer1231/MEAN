675aa8f2946bb77e7cc1131df0efc435023f0675
dreamweaver1231 <dreamweaver1231@gmail.com>
2014-07-30 11:48:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,35 +6,36 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
-
 var uri = 'mongodb://localhost/myApp';
 mongoose.connect(uri);
-
 var users = require('./routes/route.users');
-
+var products = require('./routes/route.products');
 var app = express();
 app.use(cors());
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-
 app.use('/users', users);
+app.use('/products', products);
 
-/// catch 404 and forward to error handler
+// 404 catch-all handler (middleware)
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    res.status(404);
+    res.render('404');
+});
+// 500 error handler (middleware)
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 /// error handlers
-
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if(app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -43,7 +44,6 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -53,5 +53,4 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 module.exports = app;
